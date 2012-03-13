134be9fbbc83e13ae817769962a82c01cf2db4c5
Varvara Stepanova <toivonen@yandex-team.ru>
2012-03-13 13:16:52 +0400

    Example for b-test block

---
+++ blocks/b-test/examples/10-b-test_simple.bemjson.js
@@ -0,0 +1,17 @@
+({
+    block: 'b-page',
+    title: 'Пример блока b-test',
+    head: [
+        { elem: 'css', url: '_10_b-test_simple.css' }
+    ],
+    content: [
+        {
+            block: 'b-link',
+            url: 'http://ya.ru',
+            content: 'Просто ссылка'
+        },
+        {
+            block: 'b-test'
+        }
+    ]
+})
